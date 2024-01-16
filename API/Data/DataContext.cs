abcc8b4a59e0babadba5ee8009ea654acac7a071
intial project <gayatrikhairnar130@gmail.com>
2024-01-16 12:33:16 +0530

    end of second part

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+}
