7a5ac087d552885122e5a64d44be8c030a9a9b52
intial project <gayatrikhairnar130@gmail.com>
2024-01-18 15:36:03 +0530

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,14 +12,16 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
